bdaf1b9079d2e13f49486c8ab7efbb474ca38d28
Dark_Wave\Tarang Godhari <taranggodhari@gmail.com>
2017-07-17 01:05:33 -0400

    final commit

---
+++ myLibrary/myLib/ApplicationCodeClass.cs
@@ -0,0 +1,17 @@
+﻿namespace myLib
+{
+    public class ApplicationCodeClass
+    {
+        public string combineArrayStringWithSpace(string[] stringArray)
+        {
+            string str = default(string);
+
+            foreach (string item in stringArray)
+            {
+                str += item + " ";
+            }
+
+            return str.Trim();
+        }
+    }
+}
---
+++ myLibrary/myLibraryUnitTest/UnitTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using myLib;
+
+namespace myLibraryUnitTest
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void ArrayStringWithSpaceTest()
+        {
+            ApplicationCodeClass target = new ApplicationCodeClass();
+            string[] stringArray = null;
+            string expected = string.Empty;
+            string actual;
+
+            actual = target.combineArrayStringWithSpace(stringArray);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void PositiveScenario_ArrayStringWithSpaceTest()
+        {
+
+            string expected = "Today is the wonderful day of my life";
+            string[] actualStringArray = new string[] { "Today", "is", "the", "wonderful", "day", "of", "my", "life" };
+
+            ApplicationCodeClass target = new ApplicationCodeClass();
+
+
+            string actual = target.combineArrayStringWithSpace(actualStringArray);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
